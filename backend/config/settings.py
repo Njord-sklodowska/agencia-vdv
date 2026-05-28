31877daa91e4bab16336f6f4b43522077e8347b9
sergio <218706424+Njord-sklodowska@users.noreply.github.com>
2026-05-28 17:10:33 -0300

    Configurar REST y actualizar dependencias
    S

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'agencia_vdv',
+        'USER': 'root',
+        'PASSWORD': 'agencia123654',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
