fcef2503ceb5fb64aa2ca261e1fccdf0de349acb
sergio <218706424+Njord-sklodowska@users.noreply.github.com>
2026-05-30 20:11:43 -0300

    limpieza backend y ajustes django usuario custom

--- backend/rol/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Rol(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.nombre
--- backend/usuario/models.py
+++ backend/usuario/models.py
@@ -1,11 +1,5 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from rol.models import Rol
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255)
-    rol = models.ForeignKey(Rol, on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.nombre
+class Usuario(AbstractUser):
+    pass
